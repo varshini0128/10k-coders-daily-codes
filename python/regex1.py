ea06b7c9e9b71c4cd51486f6d182a12754c00edf
varsh <varshinisiliveri22@gmail.com>
2026-02-27 16:16:53 +0530

    regular expressions

--- python/regex1.py
+++ python/regex1.py
@@ -1,3 +1,83 @@
+import regex as re 
+
+# functions
+'findall'
+d='built-in module named to work with regular expressions'
+
+res= re.findall("module",d)
+print(res)
+
+# 'match'->it checks first str with frst string of the given string
+res=re.match('ajay','ajay kumar')
+print(res)
+
+
+'fullmatch'
+res=re.fullmatch('ajay kumar','ajay kumar')
+print(res)
+
+'search'
+d='built-in module named to work with regular expressions'
+res=re.search('module',d)
+print(res)
+
+
+'sub'
+s='python@program#hello$'
+res=re.sub('[^A-Za-z0-9]',' ',s)
+print(res,"88888888888888")
+
+
+# \d - any digit from 0 to 9
+# \D - any non digit character
+# \s - any white space character
+# \S - any non white space character
+# \w - any alphanumeric character(a-z,A-Z,0-9)
+# \W - any non alphanumeric character
+
+
+
+# common regex symbols/metacharacters
+# . - any character except new line
+# * - 0 or more than 0 times
+# + - 1 or more than one time
+# $ - end of the string
+# ? - 0 or 1 time only
+# {m} - m times
+# {m,n} - m to n times
+# [a,b,c] - either a or b or c
+# [a-z] - any lowercase letter
+
+
+mobile=int(input('Enter mobile number:'))
+res=re.fullmatch('[6-9][0-9]{9}',str(mobile))
+if res:
+    print('valid mobile number')
+else:
+    print('invalid mobile number')
+
+s=' my mobile number is 7842259549'
+
+res=re.findall('\d+',s)
+print(res)
+
+
+s=' my 789 mobile 456 number is 7842259549'
+
+res=re.findall('\d+',s)
+print(res)
+
+s='my 789 mobile & $ @ 456 number is 7842259549'
+
+res=re.findall('\D',s)
+print(res,'00000000000000000000000000000000000000000000')
+
+
+# 7845129632
+
+
+
+
 26/2/2026
 
 import regex
@@ -141,5 +221,19 @@ if res:
     print('valid phone number')
 else:
     print('invalid phone number')
+
+
+# date format 'dd-mm-yyyy'
+date = input('Enter date :')
+res = regex.fullmatch('([0-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}',date)
+if res:
+    print('valid date')
+else:
+    print('invalid date')
+
+# remove special characters from string
+s='python@program#hello$'
+res=regex.sub('[A-Za-z0-9]','',s)
+print(res)
 
 
